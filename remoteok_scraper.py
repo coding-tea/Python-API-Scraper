a39598e1e3bb99aad9ecc67e99ff7f3d4bb89fbe
digital opium <test@ismail.com>
2023-12-29 18:17:05 +0100

    install dependencies

--- remoteok_scraper.py
+++ remoteok_scraper.py
@@ -1,1 +1,18 @@
-#lets start
+import requests
+import xlwt
+from xlwt import Workbook
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+BASE_URL = "https://remoteok.com/api"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
+
+def get_posting_jobs():
+    res = requests.get(url = BASE_URL)
+
+if (__name__ == "__main__"):
+    print("hello world")
